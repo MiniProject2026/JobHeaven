10c4a48a9b4bffd561289c6fb270898f27363f14
Deepak4003 <jangiddee999@gmail.com>
2026-03-17 23:39:39 +0530

    Update index.ts

--- services/auth/src/index.ts
+++ services/auth/src/index.ts
@@ -10,6 +10,9 @@ async function initDb() {
         BEGIN
             IF NOT EXISTS (SELECT 1 FROM pg_type WHERE typname = 'user_role') THEN
                 CREATE TYPE user_role AS ENUM ('jobseeker','recruiter');
+
+
+                
             END IF;
         END$$;
         `;
