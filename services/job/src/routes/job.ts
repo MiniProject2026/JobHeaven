027302ceca4eaeafebae7b79344d2406be199f63
akash6299625764 <bestakashchand@gmail.com>
2026-03-27 02:18:41 +0530

    Created job with company Id

--- services/job/src/routes/job.ts
+++ services/job/src/routes/job.ts
@@ -1,8 +1,9 @@
 import express from "express";
-import { createCompany, deleteCompany } from "../controllers/job.js";
+import { createCompany, createJob, deleteCompany } from "../controllers/job.js";
 import { isAuth } from "../middleware/auth.js";
 import uploadFile from "../middleware/multer.js";
 const router = express.Router();
 router.post("/company/new", isAuth, uploadFile, createCompany);
 router.delete("/company/:companyId", isAuth, deleteCompany);
+router.post("/new", isAuth, createJob);
 export default router;
