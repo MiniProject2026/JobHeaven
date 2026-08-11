2abaf0a9bf4f1d74e21ff15207b15f553ba9ce96
akash6299625764 <bestakashchand@gmail.com>
2026-08-11 16:20:48 +0530

    Fix TypeScript token type casting and module import extensions

--- services/user/src/middleware/auth.ts
+++ services/user/src/middleware/auth.ts
@@ -1,6 +1,6 @@
 import { NextFunction, Request, Response } from "express";
 import jwt, { JwtPayload } from "jsonwebtoken";
-import { sql } from "../utils/db";
+import { sql } from "../utils/db.js";
 
 interface user {
   user_id: string;
